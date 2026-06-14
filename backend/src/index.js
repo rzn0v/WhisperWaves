1209ff2d7190120804deb4ba37e8c34d4016268b
Reno <renovardhan@gmail.com>
2026-06-14 21:52:50 +0530

    Set up middleware for CORS and Clerk authentication

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,19 @@ import express from "express";
 import "dotenv/config";
 import User from "./models/User.js";
 import { connectDB } from "./lib/db.js";
+import { clerkMiddleware } from '@clerk/express';
+import cors from "express";
 
 const app = express()
 const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+app.use(express.json());
+app.use(cors({origin:FRONTEND_URL, credentials: true}));
+app.use(clerkMiddleware());
 
 app.get("/health", (req,res) => {
+    
     res.status(200).json({ ok: true });
 });
 
