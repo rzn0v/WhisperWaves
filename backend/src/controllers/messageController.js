23be130122c2b43f5675fdfd885e33fe06883d66
Reno <renovardhan@gmail.com>
2026-06-19 16:05:21 +0530

    Fix import path for Message model in messageController.js

--- backend/src/controllers/messageController.js
+++ backend/src/controllers/messageController.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
-import Message from "../models/Message.js";
+import Message from "../models/message.js";
 
 export async function getUsersForSidebar(req, res) {
     try{
